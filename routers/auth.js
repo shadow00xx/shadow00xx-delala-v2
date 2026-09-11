30f93de4ab7e922a0af1a5fe2b202cd9cc798825
shadow00xx <117114498+shadow00xx@users.noreply.github.com>
2026-09-11 22:21:45 +0200

    Migrate legacy router: auth

---
+++ routers/auth.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+
+// @desc    Auth with Google
+// @route   GET /auth/google
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
+
+// @desc    Google auth callback
+// @route   GET /auth/google/callback
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/user/login' }),
+  (req, res) => {
+    res.redirect('/')
+  }
+)
+
+router.get('/facebook', passport.authenticate('facebook', { scope: 'email' }));
+
+router.get(
+  '/facebook/callback',
+  passport.authenticate('facebook', {
+    failureRedirect: '/user/login',
+  }),
+  function (req, res) {
+    res.redirect('/');
+  }
+);
+
+module.exports = router
