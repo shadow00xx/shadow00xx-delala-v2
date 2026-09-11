9b064bdbbb6dc3887b63da90c9025b8225ecf4fa
shadow00xx <117114498+shadow00xx@users.noreply.github.com>
2026-09-11 22:29:41 +0200

    Restore Products model

---
+++ models/Prodects.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const ProdectsSchema = new mongoose.Schema({
+    name: { type: String, require: true }, phone: { type: Number, require: true }, address: { type: String, require: true }, body: { type: String, require: true }, prise: { type: Number, require: true },
+    image: [{ type: String }],
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    category: { type: String, enum: ['Vehicles', 'Electronics', 'Fashions', 'Realestate', 'Makup', 'ForKides', 'Foods', 'Others'], required: true },
+    cloudinary_id: { type: String }, modal: { type: String }, gas: { type: String }, conditions: { type: String, enum: ['use', 'new'] },
+    location: { type: String }, num: { type: Number }, owners: { type: String ,enum: ['owner', 'middle']}, reson: { type: String, enum: ['sale', 'rent'] },
+    comments: [{ createdby: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, text: { type: String, required: true }, avatar: { type: String }, date: { type: Date, default: Date.now } }],
+    Favorite:[{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], report:[{ type:Number }],
+}, { timestamps: true });
+ProdectsSchema.index({ name: 'text', description: 'text' });
+module.exports = mongoose.model('Prodects', ProdectsSchema)
