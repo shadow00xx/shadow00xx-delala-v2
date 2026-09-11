f1b428bf208b24c5389de3e39d6699db688a0fa9
shadow00xx <117114498+shadow00xx@users.noreply.github.com>
2026-09-11 22:22:46 +0200

    chore: align migrated admin controller with legacy

--- controllers/adminContrller.js
+++ controllers/adminContrller.js
@@ -1,20 +1,37 @@
 const User = require('../models/User')
 const prodects = require('../models/Prodects')
+
 const contacts = require('../models/Contact')
+
+
+
+
 
 exports.showreport = async (req, res) => {
     try {
+
         const pro = await prodects.find({report:444}).sort({ _id: -1 });
+
         res.render('pages/showreport', { pro })
+
     } catch (err) {
         console.log(err)
         res.render('error/500')
     }
 }
 
+
+
+
+
+
+// get
+// show showcontact 
 exports.showcontact = async(req, res)=>{
     try {
-        const contact = await contacts.find().sort('desc').populate('createby')
+const contact = await contacts.find().sort('desc')
+     .populate('createby')
+
         res.render('pages/showcontacts', {contact})
     } catch (err) {
         console.log(err);
